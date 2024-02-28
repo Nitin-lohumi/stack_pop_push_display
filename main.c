e33845213f4d93402e1aa68b13c74060eebf530d
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-02-28 22:25:07 +0530

    add linked list program

--- main.c
+++ main.c
@@ -1,61 +1,30 @@
 #include<stdio.h>
-void push();
-int top=-1;
-int arr[5];
-int main(){
-int choice;
- char n;
-do{
-printf("Enter your choice \n");
-printf("1-> for push\n 2 -> for pop\n 3->  show \n");
-scanf("%d",&choice);
- switch(choice){
-  case 1:
-      push();
-      break;
-  case 2:
-       pop();
-       break;
-  case 3:
-      show();
-      break;
-  default:
-    printf("not valid choice\n");
- }
-  printf("\n do it again enter y or N\t");
-  scanf("%s",&n);
-}while(n=='y');
+
+struct SelfReferential {
+    int data;
+    struct SelfReferential *selfPointer;
+};
+
+int main() {   
+    struct SelfReferential node1, node2, node3;
+    int data1,data2,data3;
+    printf("enter data1");
+    scanf("%d",&data1);
+    printf("enter data 2");
+    scanf("%d",&data2);
+     printf("enter data 3");
+    scanf("%d",&data3);
+   
+    node1.data = data1;
+    node2.data = data2;
+    node3.data = data3;
+
+    node1.selfPointer = &node2;
+    node2.selfPointer = &node3;
+    node3.selfPointer = NULL;  
+    printf("Node 1 data: %d\n", node1.data);
+    printf("Node 2 data: %d\n", node1.selfPointer->data);
+    printf("Node 3 data: %d\n", node1.selfPointer->selfPointer->data);
+
+    return 0;
 }
-void push(){
-    int x;
-    if(top==4){
-        printf("stack overflow\n");
-    }
-    else{
-        printf("enter the element in stack");
-        for(int i=0; i<4; i++){
-         scanf("%d",&arr[i]);
-         top=top+1;
-         arr[top]=arr[i];
-         }
-          }
-    }
-  void show(){
-        if(top==-1){
-            printf("stack have not any elements\n");
-        }
-        else{
-            printf("printing elements \n");
-            for(int i=top; i>=0; i-- ){
-                printf(" arr[%d] = %d\n ",i, arr[i]);
-            }
-        }
-    }
-    void pop(){
-        if(top==-1){
-            printf("overflow stack");
-        }
-        else{
-            top=top-1;
-        }
-    }
